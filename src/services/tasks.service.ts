def987987bfad81d2de3ae85776a05ce435ad13d
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-10-20 17:18:44 +0500

    feat: add api TasksService for get and post tasks

---
+++ src/services/tasks.service.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import {formatDate} from "../utils/utils";
+
+
+export type TaskType = {
+    id: number,
+    title: string
+    description: string,
+    status: boolean,
+    color: string,
+    date: string
+}
+type TaskAdd ={
+    titleTask:string,
+    descriptionTask:string
+}
+const { v4: uuidv4 } = require('uuid');
+
+export const TasksService ={
+    async getAll() {
+        return axios.get<TaskType[]>('http://localhost:3004/tasks')
+    },
+    async postTask(task:TaskAdd) {
+        return axios.post<TaskType>('http://localhost:3004/tasks',{
+            id: uuidv4(),
+            title: task.titleTask,
+            description: task.descriptionTask,
+            status: false,
+            color: ('#'+(Math.random()*0xFFFFFF<<0).toString(16)),
+            date: formatDate()
+        })
+    }
+};
