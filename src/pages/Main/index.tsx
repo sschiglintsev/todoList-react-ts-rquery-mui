3ad3cd5295caf574a59551b03f5926b0297f948e
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-10-20 21:41:56 +0500

    feat:add component TickerNews in Main

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './index.module.css'
-import {TodoList} from "../index";
+import {AddTask, TodoList} from "../index";
+import {TickerNews} from "../../components";
 
 export const Main = () => {
+    const [status, setStatus] = useState<boolean>(true);
+
+    const changeStatus = () => {
+        setStatus(!status)
+    };
     return (
         <div className={style.main}>
-            <TodoList/>
+            <div className={style.placeTasks}>
+                <AddTask/>
+                <TodoList status={status} changeStatus={changeStatus}/>
+            </div>
+            <TickerNews status={status}/>
         </div>
     );
 };
