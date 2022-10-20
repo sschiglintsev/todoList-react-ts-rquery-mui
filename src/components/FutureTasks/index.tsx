6008751b1a7460a2c6167c7177c20ed30207ceb9
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-10-20 21:48:42 +0500

    feat: add component FutureTasks with style

---
+++ src/components/FutureTasks/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Paper from "@mui/material/Paper";
+import style from "./index.module.css";
+import {Typography} from "@mui/material";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+
+type PropsType = {
+    title:string
+}
+
+export const FutureTasks = (props:PropsType) => {
+    return (
+        <div>
+            <Paper elevation={10} sx={{
+                marginTop: '32px',
+                marginLeft: '20px',
+                backgroundColor: '#282828',
+                borderRadius: 5,
+                width: 350,
+                height: 79,
+                display: "flex",
+                flexDirection: "column",
+                alignContent: "space-between",
+                justifyContent: "space-around",
+            }}>
+                <div className={style.taskContainer}>
+                    <div className={style.groupLT}>
+                        <div className={style.line} style={{background: `#A9A9A9`}}/>
+                        <div className={style.containerTD}>
+                            <Typography
+                                style={{
+                                    fontFamily: "Actor",
+                                    color: "#F4F4F4",
+                                    fontSize: 20,
+                                    fontWeight: 400,
+                                }}
+                            >
+                                {props.title}
+                            </Typography>
+                        </div>
+                    </div>
+                    <div className={style.checkbox}>
+                        <CheckCircleIcon sx={{
+                            color: "#F4F4F4",
+                            width: 21,
+                            height: 21,
+                        }}/>
+                    </div>
+                </div>
+            </Paper>
+
+        </div>
+    );
+};
