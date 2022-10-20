d2db92c8daf97578b64b8a687f026d25dff9d8a2
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-10-20 21:43:32 +0500

    fix: export TickerNews and FutureTasks

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,6 @@
 import {ToDayTasks} from "./ToDayTasks"
 import {Task} from "./Task"
+import {TickerNews} from "./TickerNews"
+import {FutureTasks} from "./FutureTasks"
 
-export {ToDayTasks, Task};
+export {ToDayTasks, Task, TickerNews,FutureTasks};
