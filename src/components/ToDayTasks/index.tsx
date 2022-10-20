78960407e408d9407fe887f11d6c04926fb49d5a
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-10-20 21:45:32 +0500

    feat: add function response data and change status

--- src/components/ToDayTasks/index.tsx
+++ src/components/ToDayTasks/index.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Typography} from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Checkbox from "@mui/material/Checkbox/Checkbox";
 import style from "./index.module.css"
 import {Task} from "../index";
-import Box from "@mui/material/Box";
+import {useQuery} from "react-query";
+import {TasksService} from "../../services/tasks.service";
+import CircularProgress from "@mui/material/CircularProgress";
+import Stack from "@mui/material/Stack";
 
 export const ToDayTasks = () => {
+    const {isLoading, data: response, error, isFetching} = useQuery('tasks list',
+        () => TasksService.getAll(),
+        );
+    const listTasks = response?.data.map(el => <Task key={el.title} id={el.id} title={el.title}
+                                                     description={el.description} status={el.status}
+                                                     color={el.color}/>);
+    const [status, setStatus] = useState<boolean>(true);
+
+    const changeStatus = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setStatus(event.target.checked);
+    };
     return (
         <div>
             <div className={style.checkBoxGroup}>
                 <div className={style.checkBox}>
-                    <Checkbox defaultChecked
-                              style={{
-                                  color: "#F4F4F4",
-                              }}
+                    <Checkbox
+                        checked={status}
+                        onChange={changeStatus}
+                        style={{
+                            color: "#F4F4F4",
+                        }}
                     />
                 </div>
                 <div className={style.labelCheckbox}>
@@ -25,20 +41,28 @@ export const ToDayTasks = () => {
                     </Typography>
                 </div>
             </div>
-                <Paper elevation={10} sx={{
-                           backgroundColor: '#282828',
-                           borderRadius: 5,
-                           width: 350,
-                           height: 204,
-                           display: "flex",
-                           flexDirection: "column",
-                           alignContent: "space-between",
-                           justifyContent: "space-around",
-                       }}>
-                    <Task/>
-                    <Task/>
-                    <Task/>
-                </Paper>
+            <Paper elevation={10} sx={{
+                backgroundColor: '#282828',
+                borderRadius: 5,
+                marginLeft:'20px',
+                width: 350,
+                minHeight:70,
+                display: "flex",
+                flexDirection: "column",
+                alignContent: "space-between",
+                justifyContent: "space-around",
+            }}>
+                {isLoading ?
+                    <Stack sx={{color: 'grey.500', width: 350, display: 'flex', justifyContent: 'center'}} spacing={2}
+                           direction="row">
+                        <CircularProgress color="inherit"/>
+                    </Stack>
+                    : <>
+                        {listTasks?.length?listTasks:<div className={style.labelnoTaskToday}>Time to add your first task</div>}
+                    </>}
+            </Paper>
         </div>
-    );
+    )
+        ;
 };
+
